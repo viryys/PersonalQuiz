bba9bc73c7aecf87d6bac6384db96f4e87a390db
Vadim Tatarchuk <tatarchuk.v.b@gmail.com>
2020-10-29 13:41:19 +0500

    Initial Commit

---
+++ PersonalQuiz/View Controllers/IntroductionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  IntroductionViewController.swift
+//  PersonalQuiz
+//
+//  Created by Alexey Efimov on 22.10.2020.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    @IBAction func unwind(segue: UIStoryboardSegue) {}
+}
+
---
+++ PersonalQuiz/View Controllers/ResultsViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ResultsViewController.swift
+//  PersonalQuiz
+//
+//  Created by Alexey Efimov on 26.10.2020.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    // +1. Передать сюда массив с ответами
+    // +2. Определить наиболее часто встречающийся тип животного
+    // +3. Отобразить результаты на экране
+    // +4. Избавится от кнопки возврата на предыдущий экран
+
+    @IBOutlet weak var whoAreYouLabel: UILabel!
+    @IBOutlet weak var definitionLabel: UILabel!
+    
+    var answersChoosen: [Answer] = []
+    
+    private var finalResult: [AnimalType: Int] = [
+        .dog: 0,
+        .cat: 0,
+        .turtle: 0,
+        .rabbit: 0
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.setHidesBackButton(true, animated: true)
+        
+        for answer in answersChoosen {
+            finalResult[answer.type]! += 1
+        }
+        
+        let maximumMatch = finalResult.max { a, b in a.value < b.value }
+        let yourAnimal = maximumMatch!.key
+        
+        definitionLabel.text = yourAnimal.definition
+        whoAreYouLabel.text = "Вы - \(yourAnimal.image)!"
+print("final res", finalResult)
+    }
+
+}
